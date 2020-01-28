cf5aced7d19fd4cd085643deee935290950b00bb
MarisolVillegas <60374134+MarisolVillegas@users.noreply.github.com>
2020-01-27 22:03:48 -0600

    se agrego la practica 1

---
+++ practica1/Holamundo.c
@@ -0,0 +1,8 @@
+#include"stdio.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	printf("Hola Mundo cruel 1898149");
+	return 0;
+}
